b95fc23e93c70c0b7cb043158fbb18844cb14d92
Panha <76092165+Panha13@users.noreply.github.com>
2023-08-18 19:07:15 +0700

    dw

--- resources/views/admin/slideshow/form.blade.php
+++ resources/views/admin/slideshow/form.blade.php
@@ -1,83 +1,78 @@
-@extends('admin.layouts.admin')
-@section('content')
-    <main class="content">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <form action="{{ isset($slideshow) ? route('admin.slideshow.update') : route('admin.slideshow.add') }}"
-                    method="POST" enctype="multipart/form-data">
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        <form action="{{ isset($slideshow) ? route('admin.slideshow.update') : route('admin.slideshow.add') }}"
+            method="POST" enctype="multipart/form-data">
 
-                    {{-- <form action="{{route('admin.slideshow.update')}}" method="POST" enctype="multipart/form-data"> --}}
-                    @if (!@isset($slideshow))
-                        <h3 class="mt-3">Add Slideshow</h3>
-                    @else
-                        <h3 class="mt-3">Edit Slideshow</h3>
-                        <input type="hidden" name="txtssid" value="{{ $slideshow['ssid'] }}" />
-                    @endif
+            {{-- <form action="{{route('admin.slideshow.update')}}" method="POST" enctype="multipart/form-data"> --}}
+            @if (!@isset($slideshow))
+                <h3 class="mt-3">Add Slideshow</h3>
+            @else
+                <h3 class="mt-3">Edit Slideshow</h3>
+                <input type="hidden" name="txtssid" value="{{ $slideshow['ssid'] }}" />
+            @endif
 
-                    @csrf
+            @csrf
 
-                    <div class="form-group">
-                        <label for="txttitle">Title</label>
-                        <input type="text" class="form-control"
-                            value="{{ isset($slideshow) ? $slideshow['title'] : '' }}" id="txttitle" name="txttitle"
-                            placeholder="Title">
-                        @error('txttitle')
-                            <div class="text-danger">Required</div>
-                        @enderror
-                    </div>
-
-                    <div class="form-group">
-                        <label for="txtsubtitle">Subtitle</label>
-                        <input type="text" class="form-control"
-                            value="{{ isset($slideshow) ? $slideshow['subtitle'] : '' }}" id="txtsubtitle"
-                            name="txtsubtitle" placeholder="Subtitle">
-                        @error('txtsubtitle')
-                            <div class="text-danger">Required</div>
-                        @enderror
-                    </div>
-
-                    <div class="form-group">
-                        <label for="tatext">Text</label>
-                        <textarea class="form-control" id="tatext" name="tatext"rows="3">{{ isset($slideshow) ? $slideshow['text'] : '' }}</textarea>
-                        @error('tatext')
-                            <div class="text-danger">Required</div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="txtlink">Link</label>
-                        <input type="text" class="form-control" value="{{ isset($slideshow) ? $slideshow['link'] : '' }}"
-                            id="txtlink" name="txtlink" placeholder="Link">
-                        @error('txtlink')
-                            <div class="text-danger">Required</div>
-                        @enderror
-                    </div>
-
-                    <div class="form-group ms-4">
-                        <div class="form-check form-switch ">
-                            <input class="form-check-input" type="checkbox" id="chkenable" name="chkenable"
-                                {{ isset($slideshow) ? ($slideshow['enable'] == '1' ? 'checked' : '') : 'checked' }}>
-                            <label class="form-check-label" for="chkenable">Enable</label>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="fileimg">Images</label>
-                        <input class="form-control" type="file" id="fileimg" name="fileimg">
-                        @error('fileimg')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    @if (isset($slideshow))
-                        <div class="form-group">
-                            <img src={{ URL::asset('img/slideshows/thumbnail/' . $slideshow->img) }}>
-                            <p>{{ $slideshow->img }}</p>
-                        </div>
-                    @endif
-                    <input type="submit" class="btn btn-primary mb-5"
-                        value="{{ isset($slideshow) ? 'Update slideshow' : 'Add Slideshow' }}" />
-                    <a href="{{ route('admin.slideshow') }}" class="btn btn-secondary mb-5">Cancel</a>
-
-                </form>
+            <div class="form-group">
+                <label for="txttitle">Title</label>
+                <input type="text" class="form-control"
+                    value="{{ isset($slideshow) ? $slideshow['title'] : '' }}" id="txttitle" name="txttitle"
+                    placeholder="Title">
+                @error('txttitle')
+                    <div class="text-danger">Required</div>
+                @enderror
             </div>
-        </div>
-    </main>
-@endsection
+
+            <div class="form-group">
+                <label for="txtsubtitle">Subtitle</label>
+                <input type="text" class="form-control"
+                    value="{{ isset($slideshow) ? $slideshow['subtitle'] : '' }}" id="txtsubtitle"
+                    name="txtsubtitle" placeholder="Subtitle">
+                @error('txtsubtitle')
+                    <div class="text-danger">Required</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="tatext">Text</label>
+                <textarea class="form-control" id="tatext" name="tatext"rows="3">{{ isset($slideshow) ? $slideshow['text'] : '' }}</textarea>
+                @error('tatext')
+                    <div class="text-danger">Required</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="txtlink">Link</label>
+                <input type="text" class="form-control" value="{{ isset($slideshow) ? $slideshow['link'] : '' }}"
+                    id="txtlink" name="txtlink" placeholder="Link">
+                @error('txtlink')
+                    <div class="text-danger">Required</div>
+                @enderror
+            </div>
+
+            <div class="form-group ms-4">
+                <div class="form-check form-switch ">
+                    <input class="form-check-input" type="checkbox" id="chkenable" name="chkenable"
+                        {{ isset($slideshow) ? ($slideshow['enable'] == '1' ? 'checked' : '') : 'checked' }}>
+                    <label class="form-check-label" for="chkenable">Enable</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fileimg">Images</label>
+                <input class="form-control" type="file" id="fileimg" name="fileimg">
+                @error('fileimg')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            @if (isset($slideshow))
+                <div class="form-group">
+                    <img src={{ URL::asset('img/slideshows/thumbnail/' . $slideshow->img) }}>
+                    <p>{{ $slideshow->img }}</p>
+                </div>
+            @endif
+            <input type="submit" class="btn btn-primary mb-5"
+                value="{{ isset($slideshow) ? 'Update slideshow' : 'Add Slideshow' }}" />
+            <a href="{{ route('admin.slideshow') }}" class="btn btn-secondary mb-5">Cancel</a>
+
+        </form>
+    </div>
+</div>
---
+++ resources/views/admin/slideshow/slideshowPage.blade.php
@@ -0,0 +1,58 @@
+<div class="container-fluid p-0">
+    <h1 class="h3 mb-3 float-start">Slideshow</h1>
+    <a href="{{ route('admin.slideshow.form') }}" class="btn btn-primary float-end">Add Slideshow</a>
+    <div style="clear:both"></div>
+    @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    @if (session('fail'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('fail') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    <table class="table">
+        <thead>
+            <th>No</th>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Subtitle</th>
+            <th>Text</th>
+            <th>Link</th>
+            <th>Action</th>
+        </thead>
+        <tbody id="slideshowBody">
+        </tbody>
+
+    </table>
+    <div class="d-flex justify-content-center" id="pagenation">
+        {{-- pagenation --}}
+    </div>
+    {{-- modal --}}
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" onclick="hidemodal()" aria-label="Close">
+                <span class="close" data-dismiss="modal">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              Are sure want to delete this slideshow? 
+            </div>
+            <div class="modal-footer">                    
+              <button type="button" class="btn btn-primary" id="deleteSlideshow">Yes</button>
+              <button type="button" class="btn btn-secondary" onclick="hidemodal()" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
